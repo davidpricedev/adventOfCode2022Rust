5e614625a3f92ddab38994c95a9a560fd52a9a25
David Price <dprice@nexben.com>
2022-12-03 10:44:38 -0600

    day1 refactor

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -9,23 +9,22 @@ pub fn run() {
 fn day1_part1() -> i32 {
     let file_path = "src/day1/input.txt";
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-    let chunks = contents.as_str().split("\n\n");
-    let sums = chunks.map(|chunk| {
-        let lines = chunk.split("\n");
-        let int_lines = lines
-            .map(|y| y.parse::<i32>())
-            .filter(|y| y.is_ok())
-            .map(|y| y.unwrap());
-        return int_lines.reduce(|acc, z| acc + z).unwrap();
-    });
-    return sums.reduce(|acc, z| if acc > z { acc } else { z }).unwrap();
+    let sums = get_sums_by_elf(contents.as_str());
+    return sums.max().unwrap();
 }
 
 fn day1_part2() -> i32 {
     let file_path = "src/day1/input.txt";
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-    let chunks = contents.as_str().split("\n\n");
-    let sums = chunks.map(|chunk| {
+    let sums = get_sums_by_elf(contents.as_str());
+    let mut vsums: Vec<_> = sums.collect_vec();
+    vsums.sort();
+    return last_n(&vsums, 3).iter().sum();
+}
+
+fn get_sums_by_elf(file_content: &str) -> impl Iterator<Item = i32> + '_ {
+    let chunks = file_content.split("\n\n");
+    return chunks.map(|chunk| {
         let lines = chunk.split("\n");
         let int_lines = lines
             .map(|y| y.parse::<i32>())
@@ -33,9 +32,6 @@ fn day1_part2() -> i32 {
             .map(|y| y.unwrap());
         return int_lines.sum();
     });
-    let mut vsums: Vec<_> = sums.collect_vec();
-    vsums.sort();
-    return last_n(&vsums, 3).iter().sum();
 }
 
 fn last_n<T>(v: &Vec<T>, n: usize) -> &[T] {
