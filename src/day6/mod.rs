3f8c0c569aaa8f2d7e05969a621ce4c7feb5293e
davidpricedev <david.price.dev@gmail.com>
2022-12-07 08:55:01 -0600

    day 6 part 2

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -5,15 +5,28 @@ const FILE_PATH: &str = "src/day6/input.txt";
 pub fn run() {
     let file_contents = fs::read_to_string(FILE_PATH).expect("Should have been able to read the file");
     println!("part1: {}", day6_part1(&file_contents));
-    // println!("part2: {}", day6_part2(file_contents));
+    println!("part2: {}", day6_part2(&file_contents));
 }
 
 fn day6_part1(file_contents: &str) -> usize {
   // despite the trim, an extra empty shows up at the beginning and end - thus the filter
+  let window_size = 4;
   let chars: Vec<&str> = file_contents.trim().split("").filter(|x| &x[..] != "").collect();
-  let windows = &chars[..].windows(4).map(|x| (x, are_all_values_unique(&x))).collect::<Vec<_>>();
+  let windows = &chars[..].windows(window_size).map(|x| (x, are_all_values_unique(&x))).collect::<Vec<_>>();
   for i in 0..windows.len() {
-    if windows[i].1 == true { return i + 4; }
+    if windows[i].1 == true { return i + window_size; }
+  }
+
+  9999
+}
+
+fn day6_part2(file_contents: &str) -> usize {
+  // despite the trim, an extra empty shows up at the beginning and end - thus the filter
+  let window_size = 14;
+  let chars: Vec<&str> = file_contents.trim().split("").filter(|x| &x[..] != "").collect();
+  let windows = &chars[..].windows(window_size).map(|x| (x, are_all_values_unique(&x))).collect::<Vec<_>>();
+  for i in 0..windows.len() {
+    if windows[i].1 == true { return i + window_size; }
   }
 
   9999
